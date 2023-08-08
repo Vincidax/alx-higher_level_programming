04f2d15e029957564914acefc67cadf84e778e5a
Vincent DUSHIME <vincentdx18@gmail.com>
2023-08-08 11:57:55 -0700

    skipping characters

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+for char_code in range(ord('a'), ord('z') + 1):
+    if chr(char_code) == 'q' or chr(char_code) == 'e':
+            continue
+    print("{}".format(chr(char_code)), end='')
