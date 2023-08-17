dec79bac15b70cf2d712f484947beede64386ae2
Vincent DUSHIME <vincentdx18@gmail.com>
2023-08-17 01:54:11 -0700

    multiply_by_2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
+
 def multiply_by_2(a_dictionary):
     result = {}
-    for x,y in a_dictionary.items():
+    for x, y in a_dictionary.items():
         result[x] = y * 2
     return result
