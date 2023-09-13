50a8a20a1fbf807b82c5f00fde6259e96d116448
Vincidax <vincentdx18@gmail.com>
2023-09-13 09:13:47 -0700

    class to json 3

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -9,7 +9,7 @@ def class_to_json(obj):
     '''obj is an instance of a Class
     Prototype: def class_to_json(obj)'''
 
-    if not hasattr(obj, '__dict__'):
+    if not hasattr(obj, '__class__'):
         raise ValueError("Input object must be an instance of a class")
 
     serializable_types = (list, dict, str, int, bool)
