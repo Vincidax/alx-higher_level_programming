0970d053c93b4dfa1bb75c0bc9f92e51a96eab32
Vincidax <vincentdx18@gmail.com>
2023-09-13 10:15:46 -0700

    student to jso

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+''' a class with a method that retrieves data'''
+
+
+import json
+
+
+class Student:
+    '''class with a method that retrieves data'''
+    def __init__(self, first_name, last_name, age):
+        '''Initializes inputs'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''a function that retrieves a dictionary represantion'''
+        result = {}
+        for key, value in self.__dict__.items():
+            result[key] = value
+
+        return result
