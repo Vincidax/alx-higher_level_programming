87505a8efec23ff3ad4f3d47783d641a68817399
Vincidax <vincentdx18@gmail.com>
2023-09-13 10:23:51 -0700

    correction N8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -21,15 +21,9 @@ def class_to_json(obj):
                     If any attribute is not serializable
                     (not of types list, dict, str, int, or bool).
     """
-    if not hasattr(obj, '__class__'):
-        raise ValueError("Input object must be instance of user-defined class")
-
-    serializable_types = (list, dict, str, int, bool)
+    
     result = {}
     for key, value in obj.__dict__.items():
-        if isinstance(value, serializable_types):
-            result[key] = value
-        else:
-            raise ValueError(f"Attribute '{key}' is not serializable")
-
+        result[key] = value
+        
     return result
