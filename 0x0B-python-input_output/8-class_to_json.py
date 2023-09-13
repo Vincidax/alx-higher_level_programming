c1f6f0275dfb79d02487727f31ee15766bfb9ec9
Vincidax <vincentdx18@gmail.com>
2023-09-13 08:59:46 -0700

    class to json 2

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -9,6 +9,8 @@ def class_to_json(obj):
     '''obj is an instance of a Class
     Prototype: def class_to_json(obj)'''
 
-    result = obj.__dict__
-    return result
+    result = {}
+    for key, value in obj.__dict__.items():
+        result[key] = value
 
+    return result
