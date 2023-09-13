5cb7fee8038aac0b326655f1d58af26c90b08697
Vincidax <vincentdx18@gmail.com>
2023-09-12 20:40:57 -0700

    append

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''a function that reads a text file'''
+
+
+def read_file(filename=""):
+    '''a function that reads a text file (UTF8) and prints it to stdout'''
+
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read())
