16d3d05889130c77ce5d11ae166d2c88fd6b1248
Vincidax <vincentdx18@gmail.com>
2023-09-13 09:22:59 -0700

    class to json 3

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -6,11 +6,26 @@ import json
 
 
 def class_to_json(obj):
-    '''obj is an instance of a Class
-    Prototype: def class_to_json(obj)'''
+    """
+    Serialize an instance of a class with serializable
+    attributes into a dictionary.
 
+    Args:
+        obj: An instance of a user-defined class with
+        serializable attributes.
+
+    Returns:
+        dict: A dictionary representation of the serializable
+        attributes of the object.
+
+    Raises:
+        ValueError: If the input object is not an instance of
+        a user-defined class.
+                    If any attribute is not serializable
+                    (not of types list, dict, str, int, or bool).
+    """
     if not hasattr(obj, '__class__'):
-        raise ValueError("Input object must be an instance of a class")
+        raise ValueError("Input object must be instance of user-defined class")
 
     serializable_types = (list, dict, str, int, bool)
     result = {}
