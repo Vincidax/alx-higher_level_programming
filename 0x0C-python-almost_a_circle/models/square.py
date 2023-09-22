cdc8ff4cd72caace71d8b1dbde776bd468083cc8
Vincidax <vincentdx18@gmail.com>
2023-09-22 14:18:43 -0700

    square arg

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,7 +7,12 @@ class Square(Rectangle):
     """This class deals with the properties of a square."""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """Initializes a Square instance."""
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Returns a string representation of the Square."""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     @property
     def size(self):
@@ -20,6 +25,22 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
-    def __str__(self):
-        """Return a string representation of the Square."""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+    def update(self, *args, **kwargs):
+        """Update the attributes of the Square instance."""
+        if args:
+            arg_names = ["id", "size", "x", "y"]
+            for i, arg in enumerate(args):
+                setattr(self, arg_names[i], arg)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns a dictionary representation of the Square."""
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y
+        }
+
