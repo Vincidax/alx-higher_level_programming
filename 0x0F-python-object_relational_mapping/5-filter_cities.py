0a5ee220c7e6e715214029f7430d098330506e01
Vincent DUSHIME <vincentdx18@gmail.com>
2023-11-19 08:04:43 +0200

    add 5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    stmt ="""
+        SELECT cities.name
+        FROM cities
+        INNER JOIN states ON cities.state_id = states.id
+        WHERE states.name LIKE %s
+        """
+    c_s = sys.argv[4]
+    cur.execute(stmt, (c_s, ))
+    rows = cur.fetchall()
+    city_names = [row[0] for row in rows]
+
+    print(', '.join(city_names))
+
+    #for row in rows:
+    #    print(row, end=', ')
+    cur.close()
+    db.close()
