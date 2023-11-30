864bd6e1c8f7f1fb539c5650971949d60e0a0212
Vincidax <vincentdx18@gmail.com>
2023-11-30 11:13:43 -0800

    101-post_json.sh 1.0.1

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # This script sends a JSON POST request with the contents of the specified file and displays the body of the response
-
-file_content=$(cat "$2")
-if jq -e . >/dev/null 2>&1 <<<"$file_content"; then
-    curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
-else
-    echo "Not a valid JSON"
-fi
+if jq -e . "$2" >/dev/null 2>&1; then curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"; else echo "Not a valid JSON"; fi
