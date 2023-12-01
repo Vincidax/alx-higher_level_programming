8416634708181be72c86f20dfd5b74b3012a4c1c
Vincidax <vincentdx18@gmail.com>
2023-12-01 09:00:13 -0800

    100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Python script that lists 10 commits (from the most recent to oldest)
+of a repository by a specified user using the GitHub API.
+"""
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+    params = {'per_page': 10}
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        commits = response.json()
+        for commit in commits:
+            sha = commit['sha']
+            author_name = commit['commit']['author']['name']
+            print(f"{sha}: {author_name}")
+    else:
+        print("Failed to retrieve commits")
