f040345f9932cb17f5e801442f0edc1c43630fcb
Vincidax <vincentdx18@gmail.com>
2023-12-01 07:20:01 -0800

    0-hbtn_status.py 1.1.0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,33 +1,16 @@
 #!/usr/bin/python3
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+
 import urllib.request
 
-
-def fetch_url(url):
-    """
-    Fetches the content from the provided URL using urllib package.
-
-    Args:
-        url (str): The URL to fetch content from.
-
-    Returns:
-        dict: A dictionary containing information about the fetched content.
-    """
-    with urllib.request.urlopen(url) as response:
-        content = response.read()  # Read content as bytes
-
-        return {
-            'type': type(content),
-            'content': content,
-            'utf8 content': content.decode('utf-8')
-        }
-
-
 if __name__ == "__main__":
-    target_url = 'https://alx-intranet.hbtn.io/status'
-
-    response_data = fetch_url(target_url)
-
-    print("Body response:")
-    print("\t- type:", response_data.get('type'))
-    print("\t- content:", response_data.get('content'))
-    print("\t- utf8 content:", response_data.get('utf8 content'))
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
